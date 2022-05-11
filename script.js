9eb07b3d2ac10f1494b8b1cbb884951e89c10713
Ellen Studer <ellenmstuder@gmail.com>
2022-05-11 18:13:27 -0500

    add appropriate button event listeners

--- script.js
+++ script.js
@@ -10,6 +10,33 @@ for (var i = 0; i < buttons; i++) {
   });
 }
 
+// function to show text value of button clicked in calc window
+function copyToTextarea(el) {
+  var text = el.textContent;
+  var textarea = document.getElementById('window');
+  textarea.value = textarea.value + text;
+  console.log(text);
+}
+
+// function to clear calc window when 'ce' button has been clicked
+function clearWindow(el) {
+  // var text = el.textContent;
+  var textarea = document.getElementById('window');
+  textarea.value = null;
+  console.log('BYE')
+}
+
+// setting up the mathematical operation
+// 1. when an operation button is clicked, store the current 
+//    value of the textarea in local storage
+
+// 2. store the math operation selected in local storage
 
 
+
+// carry out the operation
+// 1. when equals button is clicked, store the current value
+//    of the text area in local storage
+
+// 2. retrieve data from local storage and perform the operation
 
